2bb333091d92b20601cc40c6c9b0a0a5c1f78458
Tudou4161 <cef5787@gamil.com>
2020-10-09 18:45:56 +0900

    지영석models.py 수정

--- STTS_KRtutor_project/main_app/models.py
+++ STTS_KRtutor_project/main_app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class CheckProcess(models.Model):
+    user_id = models.CharField(max_length=300)
+    chap_1 = models.IntegerField()
+    chap_2 = models.IntegerField()
+    chap_3 = models.IntegerField()
+    chap_4 = models.IntegerField()
+    chap_5 = models.IntegerField()
+    chap_6 = models.IntegerField()
+    #고객 아이디
+    #챕터1
+    #챕터2
+    #챕터3
+    #챕터4
+    #챕터5
+    #챕터6
+
+class CheckProcessDetailsChap1(models.Model):
+    pass;
+
+class CheckProcessDetailsChap2(models.Model):
+    pass;
+
+class CheckProcessDetailsChap3(models.Model):
+    pass;
+
+class CheckProcessDetailsChap4(models.Model):
+    pass;
+
+class CheckProcessDetailsChap5(models.Model):
+    pass;
+
+class CheckProcessDetailsChap6(models.Model):
+    pass;
+    
