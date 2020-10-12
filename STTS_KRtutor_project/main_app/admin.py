9902a8eab8f6bae56019dd8b75564d2584288b99
Tudou4161 <cef5787@gamil.com>
2020-10-12 19:52:40 +0900

    views_models_admin수정_pm1952

--- STTS_KRtutor_project/main_app/admin.py
+++ STTS_KRtutor_project/main_app/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import CheckProcess, ConversationPracticeAnswerDB, ConversationPracticeQuestionDB, EssentialSentenceDB, ChapterNumberDB
-# Register your models here.
+from .models import CheckProcess, ConversationPracticeAnswerDB, ConversationPracticeQuestionDB, EssentialSentenceDB
 
 admin.site.register(CheckProcess)
-# admin.site.register(ChapterNumberDB)
-# admin.site.register(EssentialSentenceDB)
-# admin.site.register(ConversationPracticeAnswerDB)
-# admin.site.register(ConversationPracticeQuestionDB)
+admin.site.register(EssentialSentenceDB)
+admin.site.register(ConversationPracticeAnswerDB)
+admin.site.register(ConversationPracticeQuestionDB)
