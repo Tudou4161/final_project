2f91e237441beffaadc11e83738c4ed8c6f3db80
Tudou4161 <cef5787@gamil.com>
2020-10-12 19:31:40 +0900

    db_수정_pm1930

--- STTS_KRtutor_project/main_app/admin.py
+++ STTS_KRtutor_project/main_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import CheckProcess
+from .models import CheckProcess, ConversationPracticeAnswerDB, ConversationPracticeQuestionDB, EssentialSentenceDB, ChapterNumberDB
 # Register your models here.
 
 admin.site.register(CheckProcess)
+# admin.site.register(ChapterNumberDB)
+# admin.site.register(EssentialSentenceDB)
+# admin.site.register(ConversationPracticeAnswerDB)
+# admin.site.register(ConversationPracticeQuestionDB)
--- STTS_KRtutor_project/main_app/models.py
+++ STTS_KRtutor_project/main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 # 방법1 : 0 -> 미수료 / 1 -> 수료상태
 
@@ -17,17 +18,25 @@ class CheckProcess(models.Model):
     # def __str__(self):
     #     return f"{self.user.get_username()}"
 
-class ChapterNumberDB(models.Model):  #챕터 넘버링
-    Chap_No = models.IntegerField()
+
+# class ChapterNumberDB(models.Model):  #챕터 넘버링
+#     Chap_No = models.IntegerField()
+
 
 class EssentialSentenceDB(models.Model): #필수문장 적재
-    Chap_No = models.ForeignKey(ChapterNumberDB.Chap_No, related_name="essential_sentence", on_delete=models.CASCADE)
+    #Chap_No = models.ForeignKey(ChapterNumberDB.Chap_No, related_name="essential_sentence", on_delete=models.CASCADE)
+    Chap_No = models.IntegerField()
     Essentence_question = models.CharField(max_length=300)
 
+
 class ConversationPracticeQuestionDB(models.Model): #발화실습 문장(TTS) 데이터 적재
-    Chap_No = models.ForeignKey(ChapterNumberDB.Chap_No, related_name="conversation_question", on_delete=models.CASCADE)
+    #Chap_No = models.ForeignKey(ChapterNumberDB.Chap_No, related_name="conversation_question", on_delete=models.CASCADE)
+    Chap_No = models.IntegerField()
     Cosentence_question = models.CharField(max_length=300)
 
+
 class ConversationPracticeAnswerDB(models.Model): #발화실습 답변(STT) 데이터 적재
-    Chap_No = models.ForeignKey(ChapterNumberDB.Chap_No, related_name="conversation_answer", on_delete=models.CASCADE)
+    #Chap_No = models.ForeignKey(ChapterNumberDB.Chap_No, related_name="conversation_answer", on_delete=models.CASCADE)
+    Chap_No = models.IntegerField()
     Cosentence_answer = models.CharField(max_length=300)
+
